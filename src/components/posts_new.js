0b883f3fedab0deb405f0ad0725451a8b788bea1
clair13 <theclairvoyant1313@yahoo.gr>
2017-08-20 19:05:33 +0200

    Implement validation to the form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -27,7 +27,31 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors ={};
+
+  if (values.title.length < 3) {
+    errors.title = "Title must be at least 3 characters";
+  }
+
+  if (!values.title){
+    errors.title = "Enter a title";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+
+  return errors;
+
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
 })(PostsNew);
